6be5fcdf0c88f9f19bb43fe33018e045969d9dde
Erick Veiga <erickveigaf2009@gmail.com>
2024-01-31 21:48:51 +0000

    add libs

---
+++ vars/pipeline-maven.groovy
@@ -0,0 +1,22 @@
+pipeline {
+    agent{
+        label 'maven'
+    }
+    stages {
+        stage('Build') {
+            steps {
+                build()
+            }
+        }
+        stage('Test') {
+            steps {
+                test()
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+    }
+}
